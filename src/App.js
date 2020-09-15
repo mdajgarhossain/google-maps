19ce3e00258127ec3c2a4dd2076efb7a81124c08
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-09-16 03:06:56 +0600

    Add Marker and Infowindow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
-import { GoogleApiWrapper, Map } from "google-maps-react";
+import React, { useState } from "react";
+import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
 
 export function MapContainer(props) {
+  const [mapOptions, setMapOptions] = useState({
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  });
+
+  const onMarkerClick = (props, marker, e) => {
+    setMapOptions({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  };
+
+  const onClose = (props) => {
+    if (mapOptions.showingInfoWindow) {
+      setMapOptions({
+        ...mapOptions,
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
   const mapStyles = {
     width: "100%",
     height: "100%",
@@ -17,7 +41,18 @@ export function MapContainer(props) {
           lat: 23.461024,
           lng: 91.180645,
         }}
-      />
+      >
+        <Marker onClick={onMarkerClick} name={"Kandirpar, Cumilla"} />
+        <InfoWindow
+          marker={mapOptions.activeMarker}
+          visible={mapOptions.showingInfoWindow}
+          onClose={onClose}
+        >
+          <div>
+            <h2>{mapOptions.selectedPlace.name}</h2>
+          </div>
+        </InfoWindow>
+      </Map>
     </div>
   );
 }
