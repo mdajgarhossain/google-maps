79f5cdb857f5f215285abbd0115a0eb6a5f81525
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-09-16 01:48:19 +0600

    Implement Map and use GoogleApiWrapper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { GoogleApiWrapper, Map } from "google-maps-react";
 
-function App() {
-  return <div>map</div>;
+export function MapContainer(props) {
+  const mapStyles = {
+    width: "100%",
+    height: "100%",
+  };
+
+  return (
+    <div>
+      <Map
+        google={props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 23.461024,
+          lng: 91.180645,
+        }}
+      />
+    </div>
+  );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE_MAPS_API_KEY,
+})(MapContainer);
